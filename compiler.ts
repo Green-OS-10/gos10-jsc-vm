6b64601065017054636c43afb7b6ff55de5ccec1
NTPrograms <76801340+NTProgramsOfficial@users.noreply.github.com>
2025-03-07 00:04:17 -0500

    remove console log

--- compiler.ts
+++ compiler.ts
@@ -4,7 +4,6 @@ import visitors, { run } from "./visitors";
 import * as fs from "fs";
 
 const code = fs.readFileSync(process.argv[2]).toString();
-console.log(code);
 const ast = acorn.parse(code, { ecmaVersion: 2020 });
 
 walk.ancestor(ast, visitors);
